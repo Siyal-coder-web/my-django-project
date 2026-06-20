45a9af26305b9c5bd81d8d3d74b5bf8deed6046e
Siyal-coder-web <mhanzlasiyal@gmail.com>
2026-06-20 15:41:53 +0500

    resolved serverless app routing production

--- my_first_website/wsgi.py
+++ my_first_website/wsgi.py
@@ -2,9 +2,10 @@ import os
 import sys
 from django.core.wsgi import get_wsgi_application
 
-# Vercel runtime paths initialize karne ka sabse safe tareeqa
-sys.path.append(os.path.dirname(os.path.abspath(__file__)))
-sys.path.append(os.path.dirname(os.path.dirname(os.path.abspath(__file__))))
+# Vercel environment path adjustments
+base_path = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
+if base_path not in sys.path:
+    sys.path.append(base_path)
 
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'my_first_website.settings')
 
