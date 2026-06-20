5837f7e3ee411d8c821b209316a7d3f375561f46
Siyal-coder-web <mhanzlasiyal@gmail.com>
2026-06-20 15:51:13 +0500

    clean final standard structure deployment

--- my_first_website/wsgi.py
+++ my_first_website/wsgi.py
@@ -1,11 +1,5 @@
 import os
-import sys
 from django.core.wsgi import get_wsgi_application
-
-# Vercel environment path adjustments
-base_path = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
-if base_path not in sys.path:
-    sys.path.append(base_path)
 
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'my_first_website.settings')
 
