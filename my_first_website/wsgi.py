0c311990df391a26028aa4f945fe6a7d1ffcb638
Siyal-coder-web <mhanzlasiyal@gmail.com>
2026-06-20 19:04:17 +0500

    fix database path with auto migration in wsgi

--- my_first_website/wsgi.py
+++ my_first_website/wsgi.py
@@ -3,16 +3,16 @@ import sys
 from django.core.wsgi import get_wsgi_application
 from django.core.management import call_command
 
-# Root directory ko path mein shamil kar rahe hain
+# Path issues fix karne ke liye
 sys.path.append(os.path.dirname(os.path.dirname(__file__)))
 
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'my_first_website.settings')
 
+# Server start hote hi database tables deploy/create karega
 try:
-    # Yeh command cloud par aate hi saare missing tables (auth_user wagera) khud bana degi
     call_command('migrate', interactive=False)
 except Exception as e:
-    print("Migration Error:", e)
+    print("Auto Migration Error:", e)
 
 application = get_wsgi_application()
 app = application
