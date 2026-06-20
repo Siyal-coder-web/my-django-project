8582fff1ac11f204e5b4c4772f015b7dde17dbe5
Siyal-coder-web <mhanzlasiyal@gmail.com>
2026-06-20 17:30:57 +0500

    added absolute sys path handling in wsgi module

--- my_first_website/wsgi.py
+++ my_first_website/wsgi.py
@@ -1,5 +1,11 @@
 import os
+import sys
 from django.core.wsgi import get_wsgi_application
+
+# Vercel ko inner modules locate karwane ke liye system path definition
+path = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
+if path not in sys.path:
+    sys.path.append(path)
 
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'my_first_website.settings')
 
